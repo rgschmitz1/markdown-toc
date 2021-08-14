561766007b9db0d1ae6ce8275055a68e7b76f7c9
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-08-13 20:22:12 -0700

    Updated README.md and created md-toc.sh
    Added bash script to generate TOC from a markdown file.  This can probably be improved but works well enough for me.
    
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # markdown-toc
 Generate a table of contents for a markdown file
+
+This script is mainly for personal use to work in tandem with Google Docs to Markdown addon, https://github.com/evbacher/gd2md-html
+
+Feel free to contribute
---
+++ md-toc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Output usage message if user does not supply a markdown file
+usage() {
+	cat <<-EOF
+	Usage: $(basename $0) /path/to/markdown.md
+
+	Parsed table of contents from headers in markdown file.
+	If markdown contains '[TOC]' the generated table of contents will replace.
+	EOF
+}
+
+# Parse the markdown file for headers and generate the table of content
+toc() {
+	grep -e "^#\+ " "$MARKDOWN" | \
+		sed -e :1 -e 's/\(^\s*\)#/\1  /; t1' \
+		-e 's/   \([^ ].*\)$/* [\1](#\L\1/' \
+		-e :2 -e 's/\(#.*\) /\1-/; t2' \
+		-e :3 -e 's/\(#.*\)[^a-z0-9-]/\1/; t3' \
+		-e 's/$/)/'
+}
+
+# Check if a file was passed to the script
+if [ -z "$1" ] || [ ! -f "$1" ]; then
+	usage
+	exit 1
+fi
+
+MARKDOWN="$1"
+TEMP=$(mktemp)
+toc > $TEMP
+
+# Replace line containing [TOC] with table of contents
+sed -i -e "/\[TOC\]/ r $TEMP" \
+	-e 's/\[TOC\]/# Table of Contents/' "$MARKDOWN"
+
+# Output table of contents so the user knows script is successful
+cat $TEMP
+
+# Remove temp file
+rm $TEMP
